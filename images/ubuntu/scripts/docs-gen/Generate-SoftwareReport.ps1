283e298ab408498fe1b0ce9884bac1984408b980
hemanthmanga <hemanthmanga@github.com>
2025-04-09 10:35:36 +0530

    [Ubuntu-24] Add .Net 9 version

--- images/ubuntu/scripts/docs-gen/Generate-SoftwareReport.ps1
+++ images/ubuntu/scripts/docs-gen/Generate-SoftwareReport.ps1
@@ -231,7 +231,7 @@ $browsersTools.AddHeader("Environment variables").AddTable($(Build-BrowserWebdri
 
 # .NET Tools
 $netCoreTools = $installedSoftware.AddHeader(".NET Tools")
-$netCoreTools.AddToolVersionsListInline(".NET Core SDK", $(Get-DotNetCoreSdkVersions), "^\d+\.\d+\.\d")
+$netCoreTools.AddToolVersionsListInline(".NET Core SDK", $(Get-DotNetCoreSdkVersions), "^\d+\.\d+\.\d{3}")
 $netCoreTools.AddNodes($(Get-DotnetTools))
 
 # Databases
